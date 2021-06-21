aaf3f1ab72f1f35207a4e091c569db360d423744
Juan Manuel CABRERA <juanma.cabrera@pm.me>
2021-06-21 14:50:33 +0200

    image more suited for Gitlab

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
+printf'#######'
+echo $*
+printf'#######'
+env
+printf'#######'
+echo sh -c "/apim/scripts/apim.sh -h '${APIM_HOST}' -u '${APIM_USER}' -p '${APIM_PASS}' -port '${APIM_PORT}' -force -returnCodeMapping 10:0 $*"
 sh -c "/apim/scripts/apim.sh -h '${APIM_HOST}' -u '${APIM_USER}' -p '${APIM_PASS}' -port '${APIM_PORT}' -force -returnCodeMapping 10:0 $*"
